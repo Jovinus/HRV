976c0728e71b6ca6cb1e7c39f0e97ec97d45f7d0
KyungHyun Lee <lkh256@gmail.com>
2021-12-15 01:41:21 +0900

    Data Conversion Mat to JSON

---
+++ code/data_conversion_mat_json.py
@@ -0,0 +1,28 @@
+# %%
+import scipy.io as sio
+import numpy as np
+import pandas as pd
+import re
+import json
+from glob import glob
+pd.set_option("display.max_columns",None)
+
+# %% Read Data List in Directory
+data_list = glob('../data/0Pooled_Data/*mat')
+data_list.sort()
+
+# %% Read Matfile -> select rri data -> Save in JSON 
+SAVEPATH = '../data/RRI/'
+PATTERN = r"E(?P<subject>\d+)[_](?P<visit>\d+)[_]ECG[_]Session(?P<session>\d+)[_]hrv"
+
+for file_nm in data_list:
+    info = re.search(PATTERN, file_nm)
+    
+    rri_mat = sio.loadmat(file_nm, uint16_codec='latin1')
+    rri = {'subject':int(info.group('subject')),
+           'visit':int(info.group('visit')),
+           'session':int(info.group('session')),
+           'RRI':rri_mat['Res']['HRV'][0][0]['Data'][0][0]['RRi'][0][0][0][0].tolist()}
+    
+    with open(SAVEPATH + file_nm[-27:-4] + '.json', 'w') as fp:
+        json.dump(rri, fp, indent=4)
